d3be459460c0b4522bcccfab433fd73afb347b13
OpokuBandoh01 <nketiabandoh@gmail.com>
2026-06-19 10:56:45 +0000

    changes

--- Crop_Guardian/app/_layout.tsx
+++ Crop_Guardian/app/_layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack initialRouteName="(onboarding)">
+      <Stack >
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="(onboarding)" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
